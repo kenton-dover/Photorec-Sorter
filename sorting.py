60ea3766e28c9eb8d9da68f4f5e9e4f1266ebd03
Kenton <doverkenton@gmail.com>
2016-06-08 13:14:07 -0400

    removed duplicate code and fixed naming

--- sorting.py
+++ sorting.py
@@ -8,6 +8,8 @@ import os
 import shutil
 import sys
 
+__version__ = "4.0"
+
 
 # This will make a destination directory if it does not exist
 def check_dest(path):
@@ -25,12 +27,14 @@ def check_size(path):
     # I feel bad for using duplicate code ill fix this later
     files_in_source_dir = os.listdir(path)
     global total_size
+    global filePathList
     for file_in_source_dir in files_in_source_dir:
         new_path = os.path.join(path, file_in_source_dir)
         if os.path.isdir(new_path):
             check_size(new_path)
         else:
             total_size = total_size + os.path.getsize(new_path)
+            filePathList.append(new_path)
 
 
 def get_human_read(size):
@@ -42,35 +46,24 @@ def get_human_read(size):
     return "%.2f%s" % (size, suffixes[suffixIndex])
 
 
-# This is the recursive function that goes through the directories
-def progressor(path):
-    files_in_source_dir = os.listdir(path)
-    for file_in_source_dir in files_in_source_dir:
-        new_path = os.path.join(path, file_in_source_dir)
-        if os.path.isdir(new_path):
-            progressor(new_path)
-        else:
-            evaluate(new_path)
-
-
-def evaluate(path):
-    # if the file_extension does not exsit it creals a null value
-    filename, file_extension = os.path.splitext(path)
-    # handles the null value
-    if file_extension == '':
-        file_extension = "extensionNotFound"
-    elif file_extension[0] == '.':
-        file_extension = file_extension.lstrip('.')
-    if os.path.isdir(os.path.join(dest_file, file_extension)):
-        # print("Found the dir and copying the file: ", path)
-        shutil.copy(path, os.path.join(dest_file, file_extension))
+# This is the iterative function that goes through the list of files
+def progressor():
+    global filePathList
+    for file_in_source_dir in filePathList:
+        # if the file_extension does not exsit it creals a null value
+        filename, file_extension = os.path.splitext(file_in_source_dir)
+        # handles the null value
+        if file_extension == '':
+            file_extension = "extensionNotFound"
+        elif file_extension[0] == '.':
+            file_extension = file_extension.lstrip('.')
+        if os.path.isdir(os.path.join(dest_file, file_extension)):
+            shutil.copy(file_in_source_dir, os.path.join(dest_file, file_extension))
         # handles if there is no directory to sort into
-    else:
-        # print("Could not find the dir. Creating one.")
-        os.mkdir(os.path.join(dest_file, file_extension))
-        # print("Copying the file: ", path)
-        shutil.copy(path, os.path.join(dest_file, file_extension))
-    update_size(path)
+        else:
+            os.mkdir(os.path.join(dest_file, file_extension))
+            shutil.copy(file_in_source_dir, os.path.join(dest_file, file_extension))
+        update_size(file_in_source_dir)
 
 
 # displays how much more the program has left to go
@@ -84,14 +77,16 @@ def update_size(path):
 
 # main method
 def main():
+    global filePathList
     check_dest(dest_file)
     check_size(source_file)
+    print(filePathList)
     print("The file size is: ", get_human_read(total_size),
           "do you want to continue? (y/N)")
     cont = input()
     if cont is not "y":
         sys.exit()
-    progressor(source_file)
+    progressor()
 
 # main function I'm used to this type of programming
 if __name__ == "__main__":
@@ -99,4 +94,5 @@ if __name__ == "__main__":
     dest_file = input("Enter the destination file fill path ending with /: ")
     total_size = 0
     done_size = 0
+    filePathList = []
     main()
